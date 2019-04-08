00b90d875c3fe245c6a600b2ae5a73195414ab6d
kale6834 <49416728+kale6834@users.noreply.github.com>
2019-04-08 15:42:37 -0600

    Create Matrix.hpp
    Header for the Matrix file.

---
+++ Matrix.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+using std::string;
+
+const int HASHTABLESIZE = 30;
+
+struct location
+{
+    string place_name;
+    int indx;
+    places *next;
+}
+
+class Matrix
+{
+    public:
+        //Parameterized constructor to set all vals to zero in adjacency matrix
+        Matrix(int _n);
+        //Switching the edge from '0' to '1' between an origin and destination
+        void addEdge(int _origin, int _destination);
+        //-----------------------------------------------------------------------------------
+        //I WILL GO BACK THROUGH AND COMMENT EACH FUNCTION SO THEY ARE UNDERSTANDABLE - WAYNE
+        void inMatrix(string fileName);
+        void addEdge(int _origin, int _destination, double miles);
+        void displayAll();
+        void displaySpecific(int _origin, int _destination);
+        int findAdjIndx(string city);
+        void put(string city, int adjIndx);
+        unsigned int getHash(std::string word)
+        
+    private:
+        int numPlanes;
+        int numCities;
+        int matrixValues;
+        //adj matrix below
+        int size;
+        double **adj;
+        //hash table below
+        location hashPlaceTable[20];
+        location *head;
+        
+        const double COST = 7000.00; //$7000 is the avg cost of a private jet that can go across the united states
+                                    //we can change this to whatever seems appropriate or randomize in the future
+};
+
