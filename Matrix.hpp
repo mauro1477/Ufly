0824694e5a858295e340ee97bcf40a5fca3957ef
wawo9193 <45251334+wawo9193@users.noreply.github.com>
2019-04-19 17:32:33 -0600

    removed unnecessary functions
    refer back to matrix.cpp latest push to see new changes
       - removed put and find prototypes

--- Matrix.hpp
+++ Matrix.hpp
@@ -2,43 +2,38 @@
 #include <iostream>
 using std::string;
 
-const int HASHTABLESIZE = 30;
-
-struct location
+class MatNode
 {
-    string place_name;
-    int indx;
-    location *next;
+    public:
+        string orig_Name;
+        string dest_Name;
+        double distance;
+        double price;
 };
 
 class Matrix
 {
     public:
         //Parameterized constructor to set all vals to zero in adjacency matrix
-        Matrix(int _n);
+        Matrix(int n);
         //Switching the edge from '0' to '1' between an origin and destination
-        void addEdge(int _origin, int _destination);
+        // void addEdge(int _origin, int _destination);
+        // void addEdge(int, int, double, string, string);  
         //-----------------------------------------------------------------------------------
         //I WILL GO BACK THROUGH AND COMMENT EACH FUNCTION SO THEY ARE UNDERSTANDABLE - WAYNE
         void inMatrix(string fileName);
-        void addEdge(int _origin, int _destination, double miles);
+        void addEdge(double miles, string _origin, string _destination);
+        MatNode *findNode(string origin, string destination);
         void displayAll();
-        void displaySpecific(int _origin, int _destination);
-        int findAdjIndx(string city);
-        void put(string city, int adjIndx);
+        void displaySpecific(string _origin, string _destination);
         unsigned int getHash(std::string word);
-        int find(string city);
         
     private:
-        int numPlanes;
+        // int numPlanes;
         int numCities;
         int matrixValues;
-        //adj matrix below
-        int size;
-        double **adj;
-        //hash table below
-        location *hashPlaceTable[20];
-        location *head;
+        // int size;
+        MatNode ***adj;
         
         const double COST = 7000.00; //$7000 is the avg cost of a private jet that can go across the united states
                                     //we can change this to whatever seems appropriate or randomize in the future
